790fc1eee6472b1c87d106cec480e06c1a76d621
xmarcelo195 <66145723+xmarcelo195@users.noreply.github.com>
2022-04-06 00:31:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+logo = document.getElementById("headerHome")
+logo.innerHTML = '<img src="https://raw.githubusercontent.com/xmarcelo195/ze-chrome-extension/main/logo.jpg" title="MANN-FILTER" alt="MANN-FILTER" class="logo" width="116px">'
